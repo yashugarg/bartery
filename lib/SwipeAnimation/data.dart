5e716abf1bb68cfe6a94767ed682de332d7f9659
beast-001 <54389313+beast-001@users.noreply.github.com>
2020-01-31 16:37:58 +0530

    Initial Commit

---
+++ lib/SwipeAnimation/data.dart
@@ -0,0 +1,3 @@
+import 'package:animation_exp/SwipeAnimation/styles.dart';
+
+List imageData = [image15, image14, image13, image12, image11, image10, image9, image8, image7, image6, image5, image3, image4, image2, image1];
---
+++ lib/SwipeAnimation/styles.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+DecorationImage image1 = new DecorationImage(
+  image: new ExactAssetImage('assets/img1.png'),
+  fit: BoxFit.cover,
+);
+DecorationImage image2 = new DecorationImage(
+  image: new ExactAssetImage('assets/img2.png'),
+  fit: BoxFit.cover,
+);
+
+DecorationImage image3 = new DecorationImage(
+  image: new ExactAssetImage('assets/img3.png'),
+  fit: BoxFit.cover,
+);
+DecorationImage image4 = new DecorationImage(
+  image: new ExactAssetImage('assets/img4.png'),
+  fit: BoxFit.cover,
+);
+DecorationImage image5 = new DecorationImage(
+  image: new ExactAssetImage('assets/img5.png'),
+  fit: BoxFit.cover,
+);
+DecorationImage image6 = new DecorationImage(
+  image: new ExactAssetImage('assets/img6.png'),
+  fit: BoxFit.cover,
+);
+DecorationImage image7 = new DecorationImage(
+  image: new ExactAssetImage('assets/img7.png'),
+  fit: BoxFit.cover,
+);
+DecorationImage image8 = new DecorationImage(
+  image: new ExactAssetImage('assets/img8.png'),
+  fit: BoxFit.cover,
+);
+DecorationImage image9 = new DecorationImage(
+  image: new ExactAssetImage('assets/img9.png'),
+  fit: BoxFit.cover,
+);
+DecorationImage image10 = new DecorationImage(
+  image: new ExactAssetImage('assets/img10.png'),
+  fit: BoxFit.cover,
+);
+DecorationImage image11 = new DecorationImage(
+  image: new ExactAssetImage('assets/img11.png'),
+  fit: BoxFit.cover,
+);
+DecorationImage image12 = new DecorationImage(
+  image: new ExactAssetImage('assets/img12.png'),
+  fit: BoxFit.cover,
+);
+DecorationImage image13 = new DecorationImage(
+  image: new ExactAssetImage('assets/img13.png'),
+  fit: BoxFit.cover,
+);
+DecorationImage image14 = new DecorationImage(
+  image: new ExactAssetImage('assets/img14.png'),
+  fit: BoxFit.cover,
+);
+DecorationImage image15 = new DecorationImage(
+  image: new ExactAssetImage('assets/img15.png'),
+  fit: BoxFit.cover,
+);
+
+ImageProvider avatar1 = new ExactAssetImage('assets/avatars/avatar-1.jpg');
+ImageProvider avatar2 = new ExactAssetImage('assets/avatars/avatar-2.jpg');
+ImageProvider avatar3 = new ExactAssetImage('assets/avatars/avatar-3.jpg');
+ImageProvider avatar4 = new ExactAssetImage('assets/avatars/avatar-4.jpg');
+ImageProvider avatar5 = new ExactAssetImage('assets/avatars/avatar-5.jpg');
+ImageProvider avatar6 = new ExactAssetImage('assets/avatars/avatar-6.jpg');
